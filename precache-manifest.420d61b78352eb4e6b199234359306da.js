761ec4cf59070c3e19186d1c06ae031fc6ae3369
dalyIsaac <isaac.daly@outlook.com>
2019-07-14 07:53:06 +0000

    Updates

---
+++ precache-manifest.420d61b78352eb4e6b199234359306da.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45abfe63d764bc601f18331ca077d1cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91644964f674a0332a21",
+    "url": "/static/js/2.9bb8715b.chunk.js"
+  },
+  {
+    "revision": "281a668311d90ae5114d",
+    "url": "/static/js/main.c0cc1281.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
