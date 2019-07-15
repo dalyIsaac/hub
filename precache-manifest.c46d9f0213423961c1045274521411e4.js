b1f3e1978515c93cab8adb74c8204fcff8b9d12b
dalyIsaac <isaac.daly@outlook.com>
2019-07-15 04:45:54 +0000

    Updates

---
+++ precache-manifest.c46d9f0213423961c1045274521411e4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e46563eed07c4368009c516bc7d10b",
+    "url": "/hub/index.html"
+  },
+  {
+    "revision": "b896788718789201001b",
+    "url": "/hub/static/js/2.0ee3e9eb.chunk.js"
+  },
+  {
+    "revision": "52aa21445f33dba71cdc",
+    "url": "/hub/static/js/main.7bbeb78a.chunk.js"
+  },
+  {
+    "revision": "9a047c8fa8ec6efa63b6",
+    "url": "/hub/static/js/runtime~main.dbcf0da2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hub/precache-manifest.f7d91dfdcf0f720e943c709e43473e37.js"
+  "/hub/precache-manifest.c46d9f0213423961c1045274521411e4.js"
 );
 
 self.addEventListener('message', (event) => {
