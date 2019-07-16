db0a34e41d822ab3f96e012a3008a4715852064c
dalyIsaac <isaac.daly@outlook.com>
2019-07-16 22:05:08 +1200

    AppBar title now routes

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { mergeStyleSets, getTheme } from "@uifabric/styling";
 import { Text } from "office-ui-fabric-react";
+import { Link } from "react-router-dom";
 
 export const APPBAR_HEIGHT = 48;
 
@@ -10,9 +11,18 @@ const styles = mergeStyleSets({
     backgroundColor: theme.palette.themePrimary,
     boxShadow: "0 6px 20px 0 rgba(0, 0, 0, 0.19)",
     height: APPBAR_HEIGHT,
-    color: theme.palette.neutralLighter,
     display: "grid",
     alignItems: "center",
+  },
+  link: {
+    color: theme.palette.black,
+    textDecoration: "none",
+    selectors: {
+      "&:hover": {
+        textDecoration: "none",
+        color: theme.palette.neutralLighter,
+      },
+    },
   },
   title: {
     paddingLeft: 24,
@@ -22,9 +32,11 @@ const styles = mergeStyleSets({
 export default function(): JSX.Element {
   return (
     <div className={styles.appBar}>
-      <Text className={styles.title} variant="xLarge">
-        hub
-      </Text>
+      <Link to={"/"} className={styles.link}>
+        <Text className={styles.title} variant="xLarge">
+          hub
+        </Text>
+      </Link>
     </div>
   );
 }
