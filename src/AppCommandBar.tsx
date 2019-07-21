bfaf498b1f9e5d7228131a26dbe1b2664112a81b
dalyIsaac <isaac.daly@outlook.com>
2019-07-21 12:16:16 +1200

    Switched from CommandBar to CommandBarButtons

--- src/AppCommandBar.tsx
+++ src/AppCommandBar.tsx
@@ -1,67 +1,52 @@
 import React from "react";
 import {
-  CommandBar,
-  ICommandBarItemProps,
   getTheme,
   mergeStyleSets,
+  CommandBarButton,
 } from "office-ui-fabric-react";
 import { RouteIdProps } from "./Routing";
 import { useDispatch } from "react-redux";
-import { Dispatch } from "redux";
 import { createSubject } from "./subject/model/Create";
 
 export const APP_COMMAND_BAR_HEIGHT = 45;
 
 const theme = getTheme();
 const styles = mergeStyleSets({
-  appCommandBar: {
+  wrapper: {
     borderBottom: "1px solid " + theme.palette.neutralQuaternary,
     boxShadow: "0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "stretch",
   },
 });
-const getCommandItems = (
-  dispatch: Dispatch,
-  id?: string,
-): ICommandBarItemProps[] => {
-  const items = [];
-
-  if (id !== undefined) {
-    items.push({
-      key: "createChildSubject",
-      name: "Create child subject",
-      iconProps: {
-        iconName: "Childof",
-      },
-      ariaLabel: "Create child subject",
-      onClick: () => {
-        dispatch(createSubject({ parent: id }));
-      },
-    });
-  } else {
-    items.push({
-      key: "createSubject",
-      name: "Create subject",
-      iconProps: {
-        iconName: "Add",
-      },
-      ariaLabel: "Create subject",
-      onClick: () => {
-        dispatch(createSubject());
-      },
-    });
-  }
-
-  return items;
-};
 
 export default function({ match }: RouteIdProps): JSX.Element {
   const { id } = match.params;
   const dispatch = useDispatch();
 
-  return (
-    <CommandBar
-      items={getCommandItems(dispatch, id)}
-      className={styles.appCommandBar}
+  const dispatchCreateChildSubject = () => {
+    dispatch(createSubject({ parent: id }));
+  };
+  const dispatchCreateSubject = () => {
+    dispatch(createSubject());
+  };
+
+  const createSubjectButton = id ? (
+    <CommandBarButton
+      text="Create child subject"
+      iconProps={{ iconName: "Childof" }}
+      ariaLabel="Create child subject"
+      onClick={dispatchCreateChildSubject}
+    />
+  ) : (
+    <CommandBarButton
+      text="Create subject"
+      iconProps={{ iconName: "Add" }}
+      ariaLabel="Create subject"
+      onClick={dispatchCreateSubject}
     />
   );
+
+  return <div className={styles.wrapper}>{createSubjectButton}</div>;
 }
