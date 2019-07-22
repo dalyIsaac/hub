3738b49fc0edf669d88975f2bde9c7d73bdacf07
dalyIsaac <isaac.daly@outlook.com>
2019-07-22 20:42:01 +1200

    Add placeholder search controls

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { mergeStyleSets, getTheme } from "@uifabric/styling";
-import { Text } from "office-ui-fabric-react";
+import {
+  Text,
+  SearchBox,
+  Dropdown,
+  IDropdownOption,
+} from "office-ui-fabric-react";
 import { Link } from "react-router-dom";
 
 export const APPBAR_HEIGHT = 48;
@@ -11,8 +16,9 @@ const styles = mergeStyleSets({
     alignItems: "center",
     backgroundColor: theme.palette.themePrimary,
     boxShadow: "0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-    display: "grid",
+    display: "flex",
     height: APPBAR_HEIGHT,
+    justifyContent: "space-between",
   },
   link: {
     color: theme.palette.black,
@@ -24,12 +30,27 @@ const styles = mergeStyleSets({
       },
     },
   },
+  search: {
+    display: "flex",
+    flexDirection: "row",
+    width: "calc(100vw / 3)",
+  },
+  searchDropdown: {
+    width: 150,
+  },
   title: {
     paddingLeft: 24,
   },
 });
 
 export default function AppBar(): JSX.Element {
+  const options: IDropdownOption[] = [
+    { key: "name", text: "Name" },
+    { key: "description", text: "Description" },
+    { key: "childName", text: "Child name" },
+    { key: "childDescription", text: "Child description" },
+  ];
+
   return (
     <div className={styles.appBar}>
       <Link to={"/"} className={styles.link}>
@@ -37,6 +58,17 @@ export default function AppBar(): JSX.Element {
           hub
         </Text>
       </Link>
+
+      <div className={styles.search}>
+        <Dropdown
+          options={options}
+          defaultSelectedKey="name"
+          className={styles.searchDropdown}
+        />
+        <SearchBox placeholder="Search" />
+      </div>
+
+      <div></div>
     </div>
   );
 }
