52d13d8ed219e9d4e64e4c8907ec45446825f153
dalyIsaac <isaac.daly@outlook.com>
2019-08-17 21:32:52 +1200

    Fixed ViewsNav height

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import View from "./components/View";
 import ViewsNav from "./components/ViewsNav";
 import { mergeStyleSets } from "@uifabric/styling";
 import { subjectBase } from "./model/Subject/Routing";
+import { APPBAR_HEIGHT } from "./Common";
 
 const styles = mergeStyleSets({
   main: {
@@ -20,12 +21,13 @@ const styles = mergeStyleSets({
   wrapper: {
     display: "flex",
     flexDirection: "row",
+    height: `calc(100vh - ${APPBAR_HEIGHT}px)`,
   },
 });
 
 const App: React.FC = (): JSX.Element => {
   return (
-    <div>
+    <React.Fragment>
       <Route path={AllPaths} component={AppBar} />
       <div className={styles.wrapper}>
         <Route path={AllPaths} component={ViewsNav} />
@@ -38,7 +40,7 @@ const App: React.FC = (): JSX.Element => {
           </Switch>
         </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
